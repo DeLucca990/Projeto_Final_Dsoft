6ba97ac73a38a32729f9e678f799d015d79f0b92
DeLucca990 <pedruca2001@gmail.com>
2022-05-20 22:16:20 -0300

    adicionando o pulo e diferentes dureções para o personagem

--- jogo_pygame.py
+++ jogo_pygame.py
@@ -23,10 +23,14 @@ timer=0
 clock=pygame.time.Clock()
 while game:
     clock.tick(60)
+
+    #Eventos:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game=False
-
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_SPACE:
+                jogador.pular()
     #Update Lógica:
     grupo_de_objeto.update()
     timer+=1
@@ -34,7 +38,6 @@ while game:
         timer=0
         if random.random()<0.3:
             novo_quizz=Quizz(grupo_de_objeto)
-
     #Draw:
     window.fill((255,255 ,255))
     grupo_de_objeto.draw(window)
--- personagens_class.py
+++ personagens_class.py
@@ -3,13 +3,27 @@ import pygame
 class Personagem(pygame.sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
-        self.lado='direita'
-        self.image=pygame.image.load('img/pulga_direita.png')
+        self.lado='parado'
+        self.image=pygame.image.load('img/pulga_parado.png')
         self.image=pygame.transform.scale(self.image,[80,80])
-        self.rect=pygame.Rect(0,395,100,100)
+        self.rect=pygame.Rect(0,600,100,100)
+        self.pulo=False
+        self.posy_inicial=600
+
+    def pular(self):
+        self.pulo=True
     
     def update(self, *args):
         #Lógica
+        if self.pulo==True:
+            if self.rect.y<=400:
+                self.pulo=False
+            self.rect.y-=5
+        else:
+            if self.rect.y<self.posy_inicial:
+                self.rect.y+=5
+            else:
+                self.rect.y=self.posy_inicial
         keys=pygame.key.get_pressed()
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.lado='esquerda'
@@ -21,16 +35,17 @@ class Personagem(pygame.sprite.Sprite):
             self.image=pygame.image.load('img/pulga_direita.png')
             self.image=pygame.transform.scale(self.image,[80,80])
             self.rect.x += 5
-        elif keys[pygame.K_w] or keys[pygame.K_UP]:
-            self.rect.y -= 5
-        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            self.rect.y += 5
-
+        else:
+            self.lado='parado'
+            self.image=pygame.image.load('img/pulga_parado.png')
+            self.image=pygame.transform.scale(self.image,[80,80])
+            
         if self.rect.top<0:
             self.rect.top=0
         elif self.rect.bottom>620:
             self.rect.bottom=620
-        elif self.rect.left<-20:
-            self.rect.left=-20
+        elif self.rect.left<50:
+            self.rect.left=50
         elif self.rect.right>1060:
             self.rect.right=1060
+            
