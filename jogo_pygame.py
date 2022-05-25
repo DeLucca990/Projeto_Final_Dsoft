f23c63ba0500a2c4d39c79d54697949874affe91
DeLucca990 <pedruca2001@gmail.com>
2022-05-25 11:16:36 -0300

    Consertando a física entre o jogador e a plataforma

--- jogo_pygame.py
+++ jogo_pygame.py
@@ -17,19 +17,19 @@ pygame.display.set_caption('Dsoft Kong')
 grupo_de_play=pygame.sprite.Group()
 grupo_de_ep=pygame.sprite.Group()
 grupo_de_quizz=pygame.sprite.Group()
-grupo_de_vida=pygame.sprite.Group()
+grupo_de_monitores=pygame.sprite.Group()
 grupo_de_plataforma=pygame.sprite.Group()
                          #ini  #fim #gap
-for pos_inicial in range (100,200,150):
+for pos_inicial in range (100,600,150):
     pos_x=random.randint(10,900)
     plat_movel=Plataforma(pos_x,pos_inicial)
     grupo_de_plataforma.add(plat_movel)
 
-plat_fix=Plataforma2()
-grupo_de_plataforma.add(plat_fix)
 jogador=Personagem()
 grupo_de_play.add(jogador)
 
+#Itens relevantes:
+vidas=5
 game=True
 timer=0
 clock=pygame.time.Clock()
@@ -67,34 +67,23 @@ while game:
     timer+=1
     if timer>20:
         timer=0
-        if random.random()<0.3:
+        if random.random()<0.2:
             novo_quizz=Quizz(grupo_de_play,grupo_de_quizz)
-        if random.random()<0.1:
+        if random.random()<0.05:
             novo_ep1=Ep1(grupo_de_play,grupo_de_ep)
             novo_ep2=Ep2(grupo_de_play,grupo_de_ep)
         if random.random()<0.001:
-            novo_pedro=Pedro(grupo_de_play,grupo_de_vida)
-            novo_marcio=Marcio(grupo_de_play,grupo_de_vida)
+            novo_pedro=Pedro(grupo_de_play,grupo_de_monitores)
+            novo_marcio=Marcio(grupo_de_play,grupo_de_monitores)
 
     #Colisões:
     colisao_jogador_quizz=pygame.sprite.spritecollide(jogador,grupo_de_quizz,True,pygame.sprite.collide_mask)
     colisao_jogador_ep=pygame.sprite.spritecollide(jogador,grupo_de_ep,True,pygame.sprite.collide_mask)
-    colisao_jogador_vida=pygame.sprite.spritecollide(jogador,grupo_de_vida,True,pygame.sprite.collide_mask)
+    colisao_jogador_vida=pygame.sprite.spritecollide(jogador,grupo_de_monitores,True,pygame.sprite.collide_mask)
     colisao_jogador_mesa=pygame.sprite.spritecollide(jogador,grupo_de_plataforma,False)
+    #Colisão plataforma móvel:
     tolerancia_colisao=10
-    if colisao_jogador_mesa:
-        #plataforma fixa
-        if abs(jogador.rect.left - plat_fix.rect.right)<tolerancia_colisao:
-            jogador.vel_x=0
-            jogador.rect.left=plat_fix.rect.right
-        if abs(jogador.rect.top - plat_fix.rect.bottom)<tolerancia_colisao:
-            jogador.vel_y=1
-        if abs(jogador.rect.bottom-plat_fix.rect.top)<tolerancia_colisao:
-            jogador.vel_y=0
-        if abs(jogador.rect.right-plat_fix.rect.left)<tolerancia_colisao:
-            jogador.vel_x=0
-            jogador.rect.right=plat_fix.rect.left
-        #plataforma movel
+    for plat_movel in colisao_jogador_mesa:
         if abs(jogador.rect.left - plat_movel.rect.right)<tolerancia_colisao:
             jogador.vel_x=0
             jogador.rect.left=plat_movel.rect.right
@@ -103,12 +92,24 @@ while game:
         if abs(jogador.rect.bottom-plat_movel.rect.top)<tolerancia_colisao:
             jogador.rect.y-=2
             jogador.vel_y=2
+            jogador.vel_x=1
             jogador.rect.x+=plat_movel.vel_x
         if abs(jogador.rect.right-plat_movel.rect.left)<tolerancia_colisao:
             jogador.vel_x=0
             jogador.rect.right=plat_movel.rect.left
+    #Colisão quizz:
+    if len(colisao_jogador_quizz)>0:
+        vidas-=1
 
-    #Draw:s
+    #Colisão eps:
+    if len(colisao_jogador_ep)>0:
+        vidas-=2
+
+    #Colisão monitores:
+    if len(colisao_jogador_vida)>0:
+        vidas+=1
+
+    #Draw:
     window.fill((4,71 ,13))
     grupo_de_plataforma.draw(window)
     grupo_de_play.draw(window)
